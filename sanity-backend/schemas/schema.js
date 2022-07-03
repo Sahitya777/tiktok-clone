e88aa7665a2f3ef5da49eba06c0be5de1f9c245c
Sahitya77 <sahityanijhawan@gmail.com>
2022-07-03 15:16:55 +0530

    schemas updated

--- sanity-backend/schemas/schema.js
+++ sanity-backend/schemas/schema.js
@@ -3,7 +3,10 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
-
+import post from './post'
+import user from './user'
+import comment from './comment'
+import postedBy from './postedBy'
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
@@ -12,5 +15,6 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
+    post,user,comment,postedBy
   ]),
 })
