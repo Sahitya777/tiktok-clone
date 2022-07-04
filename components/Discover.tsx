0f8075696a22541f36f49067c6a759d7036ff8ad
Sahitya77 <sahityanijhawan@gmail.com>
2022-07-04 16:16:40 +0530

    discover done

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -3,6 +3,10 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { topics } from '../utils/constants'
 const Discover = () => {
+    const router=useRouter();
+    const { topic } = router.query;
+    const activeTopicStyle = 'xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#F51997]';
+    const topicStyle = 'xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black';
   return (
     <div className='xl:border-b-2 xl:border-gray-200 pb-6'>
         <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block'>
@@ -11,11 +15,11 @@ const Discover = () => {
         <div className='flex gap-3 flex-wrap'>
             {topics.map((item)=>(
                 <Link href={`/?topic=${item.name}`} key={item.name}>
-                    <div>
-                        <span>
+                    <div className={topic==item.name ? activeTopicStyle: topicStyle}>
+                        <span className='font-bold text-2xl xl:text-md'>
                             {item.icon}
                         </span>
-                        <span>
+                        <span className='font-medium text-md hidden xl:block capitalize'>
                             {item.name}
                         </span>
                     </div>
