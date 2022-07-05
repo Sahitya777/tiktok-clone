3a85996739b9473ab51c148e2b7f08c00184d6ec
Sahitya77 <sahityanijhawan@gmail.com>
2022-07-05 16:33:31 +0530

    google auth added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,8 +5,10 @@ import { useRouter } from 'next/router'
 import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi'
 import {IoMdAdd} from 'react-icons/io'
+import { GoogleLogin,googleLogout } from '@react-oauth/google';
 import Logo from '../utils/tiktik-logo.png';
 const Navbar = () => {
+  const user=false;
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
         <Link href="/">
@@ -19,6 +21,19 @@ const Navbar = () => {
                 />
             </div>
         </Link>
+        <div>
+          SEARCH
+        </div>
+        <div>
+          {user ?(
+            <div>LOGGED IN</div>
+          ):(
+            <GoogleLogin
+              onSuccess={(response)=>console.log(response)}
+              onError={()=>console.log('error')}
+            />
+          )}
+        </div>
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ const MyApp=({ Component, pageProps }: AppProps)=> {
   if(isSSR) return null;
   
   return(
-    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_API_TOKEN}`}>
+    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
       <Navbar/>
       <div className='flex gap-6 md:gap-20'>
         <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
