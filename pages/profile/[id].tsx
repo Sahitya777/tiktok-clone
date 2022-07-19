fd55f23216b1621aaaa720d72efb9e4ba7441aca
Sahitya77 <sahityanijhawan@gmail.com>
2022-07-19 22:13:38 +0530

    id

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,6 +1,24 @@
+import React,{useEffect} from "react";
+import Image from "next/image";
+import Link from "next/link";
+import axios from "axios";
+import { GoVerified } from "react-icons/go";
+import VideoCard from "../../components/VideoCard";
+import NoResults from "../../components/NoResults";
+import { BASE_URL } from "../../utils";
+import {IUser,Video} from '../../types'
+
+
 const Profile=()=>{
     return(
         <div>Profile</div>
     )
 }
+
+export const getServerSideProps=async ({params:{id}}:{
+    params:{id:string}
+})=>{
+    const res=await axios.get(`${BASE_URL}/proifle/${id}`)
+}
+
 export default Profile;
