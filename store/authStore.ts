393a81fc947d4e655207341a0fb176771954e21d
Sahitya77 <sahityanijhawan@gmail.com>
2023-07-31 00:11:53 +0530

    github actions setup

--- store/authStore.ts
+++ store/authStore.ts
@@ -2,6 +2,7 @@ import create from "zustand";
 import {persist} from 'zustand/middleware'
 import axios from 'axios';
 import { BASE_URL } from "../utils";
+
 const authStore=(set:any)=>({
     userProfile:null,
     allUsers:[],
